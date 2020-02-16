071b9de7206184170cca3c0e0b2984e8d44f5ad4
Z Naddaf <znaddaf@protonmail.com>
2020-02-16 14:06:22 -0600

    still updating routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,7 +5,7 @@
 // Dependencies
 // =============================================================
 var path = require("path");
-var workoutCollection = require("../models/exercise")
+var workoutCollection = require("../models")
 
 // Routes
 // =============================================================
@@ -27,55 +27,54 @@ module.exports = function (app) {
       res.json(newWorkout)
     })
   });
-};
 
-// Get/Read route for getting all workouts.
-app.get("/api/workouts", function (req, res) {
-  // getting it out of Workout.js in models.
-  db.Workout.find({}).then(function (dbWorkout) {
-    res.json(dbWorkout);
-  }).catch(err => {
-    res.json(err);
-  })
-})
-app.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({}).then(function (dbWorkout) {
-    res.json(dbWorkout);
-  }).catch(err => {
-    res.json(err);
-  });
-})
-// Get single workout by id.
-app.get("/api/workouts/:id", function (req, res) {
-  var id = req.params.id;
-  db.Workout.findById(id, function (err, dbWorkout) {
-    if (err) {
-      console.error(err)
-    }
-    res.json(dbWorkout);
-  })
-})
-// Post/Create a new workout in the database.
-app.post("/api/workouts/", function (req, res) {
-  db.Workout.create({ exercise: req.body }).then(function (dbWorkout) {
-    res.json(dbWorkout);
-  }).catch(err => {
-    res.json(err);
-  })
-})
-
-// Update a workout with an exercise in the database by id.
-app.put("/api/workouts/:id", function (req, res) {
-  var query = { _id: req.params.id };
-  db.Workout.findOneAndUpdate(query, {
-    $push: { exercises: [req.body] }
-  }, function (err, dbWorkout) {
-    if (err) {
-      res.json(err);
-    } else {
+  // Get/Read route for getting all workouts.
+  app.get("/api/workouts", function (req, res) {
+    // getting it out of Workout.js in models.
+    db.Workout.find({}).then(function (dbWorkout) {
       res.json(dbWorkout);
-    }
+    }).catch(err => {
+      res.json(err);
+    })
   })
-})
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).then(function (dbWorkout) {
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.json(err);
+    });
+  })
+  // Get single workout by id.
+  app.get("/api/workouts/:id", function (req, res) {
+    var id = req.params.id;
+    db.Workout.findById(id, function (err, dbWorkout) {
+      if (err) {
+        console.error(err)
+      }
+      res.json(dbWorkout);
+    })
+  })
+  // Post/Create a new workout in the database.
+  app.post("/api/workouts/", function (req, res) {
+    db.Workout.create({ exercise: req.body }).then(function (dbWorkout) {
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.json(err);
+    })
+  })
 
+  // Update a workout with an exercise in the database by id.
+  app.put("/api/workouts/:id", function (req, res) {
+    var query = { _id: req.params.id };
+    db.Workout.findOneAndUpdate(query, {
+      $push: { exercises: [req.body] }
+    }, function (err, dbWorkout) {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(dbWorkout);
+      }
+    })
+  })
 
+}
